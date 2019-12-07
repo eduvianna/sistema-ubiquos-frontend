7ec8e8df1d75e97492f2ffc2ef8aacdb01d07a71
eduvianna <eduardocosta_viana@live.com>
2019-12-06 21:54:31 -0300

    Adicionado funcionalidade SignIn e SignUp

---
+++ src/routes/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Switch } from 'react-router-dom';
+import Route from './Routes';
+
+// Imports Register and Login
+import SignIn from '../pages/SignIn';
+import SignUp from '../pages/SignUp';
+
+// Imports Dashboard
+import Dashboard from '../pages/Dashboard';
+import Profile from '../pages/Profile';
+import Project from '../pages/Project';
+
+export default function Routes() {
+  return (
+    <Switch>
+      <Route path="/" exact component={SignIn} />
+      <Route path="/register" component={SignUp} />
+      <Route path="/dashboard" component={Dashboard} />
+      <Route path="/profile" component={Profile} />
+      <Route path="/project" component={Project} />
+    </Switch>
+  );
+}
